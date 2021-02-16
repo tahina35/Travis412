f88e4df2f5ea437f852c729dc99c67ddc1d57670
Tahina <andriantomangat@gmail.com>
2021-02-16 17:52:29 -0500

    1. running the test for 2 * 2 = 3 (wrong test) 2. running the test for 10 / 5 = 2 (correct test)

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -7,4 +7,10 @@ public class SimpleCalculator {
 	public int minus(int a, int b) {
 		return a - b;
 	}
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	public int divide(int a, int b) {
+		return a / b;
+	}
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -17,4 +17,16 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(5, 3), 2);
 	}
+
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(2, 2), 3);
+	}
+
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(10, 5), 2);
+	}
 }
